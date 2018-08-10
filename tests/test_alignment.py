1b33ffc17abd2c2a2e5a43c082735e41b77402b5
arvestad <arve@csc.kth.se>
2018-08-10 15:16:59 +0200

    Added some tests and a test data file (t4.fa)

--- tests/test_alignment.py
+++ tests/test_alignment.py
@@ -6,18 +6,23 @@ from Bio.Align import MultipleSeqAlignment
 from alv.alignment import aaAlignment
 
 class TestAlignment(unittest.TestCase):
-    def test_summarize_columns(self):
+    def setUp(self):
         simple_al = MultipleSeqAlignment([
              SeqRecord(Seq("AR", generic_protein), id="Alpha"),
-             SeqRecord(Seq("AR", generic_protein), id="Beta"),
+             SeqRecord(Seq("AR", generic_protein), id="Beta_"),
              SeqRecord(Seq("AS", generic_protein), id="Gamma"),
          ])
-        al = aaAlignment(simple_al)
-        self.assertEqual(al._summarize_columns()[0]['A'], 3)
-        self.assertEqual(al._summarize_columns()[1]['R'], 2)
-        self.assertEqual(al._summarize_columns()[1]['S'], 1)
-        self.assertEqual(al._summarize_columns()[1]['A'], 0)
+        self.al = aaAlignment(simple_al)
+ 
+    def test_summarize_columns(self):
+        self.assertEqual(self.al._summarize_columns()[0]['A'], 3)
+        self.assertEqual(self.al._summarize_columns()[1]['R'], 2)
+        self.assertEqual(self.al._summarize_columns()[1]['S'], 1)
+        self.assertEqual(self.al._summarize_columns()[1]['A'], 0)
 
+    def test_accession_length(self):
+        for acc in self.al.accessions():
+            self.assertEqual(len(acc), 5)
 
 if __name__ == '__main__':
     unittest.main()
--- tests/test_io.py
+++ tests/test_io.py
@@ -1,9 +1,12 @@
 import unittest
+import argparse
 from Bio.Alphabet import generic_dna, generic_protein
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
 from Bio.Align import MultipleSeqAlignment
 import alv.io
+from alv.alignment import aaAlignment, dnaAlignment, codonAlignment, BaseAlignment
+import alv.colorize
 
 class TestIO_etc(unittest.TestCase):
     def test_guess_seq_type(self):
@@ -27,6 +30,32 @@ class TestIO_etc(unittest.TestCase):
         self.assertEqual(alv.io.guess_seq_type(noncoding), 'dna')
         self.assertEqual(alv.io.guess_seq_type(peptide), 'aa')
 
+
+class TestFormats(unittest.TestCase):
+    def setUp(self):
+        self.dna_filename = 'tests/t1.fa'
+        self.aa_filename = 'tests/t4.fa'
+
+    def test_reading_files(self):
+        al, painter = alv.io.read_alignment(self.dna_filename, 'dna', 'fasta', '', 'standard')
+        self.assertIsInstance(al, dnaAlignment)
+        self.assertIsInstance(painter, alv.colorize.dnaPainter)
+        al, painter = alv.io.read_alignment(self.dna_filename, 'aa', 'fasta', 'clustal', 'standard')
+        self.assertIsInstance(al, aaAlignment)
+        self.assertIsInstance(painter, alv.colorize.aaPainter)
+        al, painter = alv.io.read_alignment(self.dna_filename, 'codon', 'fasta', 'clustal', 'standard')
+        self.assertIsInstance(al, codonAlignment)
+        self.assertIsInstance(painter, alv.colorize.codonPainter)
+
+    def test_guessing_seq_type(self):
+        '''
+        Given simple DNA or AA input, is the sequence typ (DNA/AA) guessed correctly?
+        '''
+        al, painter = alv.io.read_alignment(self.dna_filename, 'guess', 'fasta', '', 'standard')
+        self.assertIsInstance(al, dnaAlignment)
+        al, painter = alv.io.read_alignment(self.aa_filename, 'guess', 'fasta', '', 'standard')
+        self.assertIsInstance(al, aaAlignment)
+        
 if __name__ == '__main__':
     unittest.main()
     
